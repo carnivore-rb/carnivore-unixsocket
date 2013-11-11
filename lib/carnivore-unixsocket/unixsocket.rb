295f5e8679f621617295c877187e490fb48b34dc
chrisroberts <chrisroberts.code@gmail.com>
2013-11-11 14:04:11 -0800

    Setup server when processing. Connect to socket for transmits.

--- lib/carnivore-unixsocket/unixsocket.rb
+++ lib/carnivore-unixsocket/unixsocket.rb
@@ -1,35 +1,50 @@
 require 'carnivore'
-require 'socket'
-require 'carnivore-unixsocket/util/socket_fetcher'
+require 'carnivore-unixsocket/util/socket_server'
 
 module Carnivore
   class Source
     class UnixSocket < Source
 
-      attr_reader :socket, :fetcher_name
+      attr_reader :socket, :srv_name, :init_args
 
       def setup(args={})
-        @socket = File.expand_path(args[:file])
-        @fetcher_name = "socket_fetcher_#{name}".to_sym
-        callback_supervisor.supervise_as(fetcher_name, Carnivore::UnixSocket::Util::Fetcher,
-          args.merge(:notify_actor => current_actor)
+        @socket = ::File.expand_path(args[:path])
+        @srv_name = "socket_srv_#{name}".to_sym
+        @connection = nil
+        @init_args = args
+      end
+
+      def server
+        Celluloid::Actor[srv_name]
+      end
+
+      def connection
+        unless(@connection)
+          @connection = Celluloid::IO::UnixSocket.new(socket)
+        end
+        @connection
+      end
+
+      def init_srv
+        callback_supervisor.supervise_as(srv_name,
+          Carnivore::UnixSocket::Util::Server,
+          init_args.merge(:notify_actor => current_actor)
         )
-      end
-
-      def fetcher
-        Celluloid::Actor[fetcher_name]
-      end
-
-      def connect
-        fetcher.async.start_fetcher
+        server.async.start
       end
 
       def receive(*args)
         wait(:new_socket_lines)
+        server.return_lines
       end
 
       def transmit(payload, original_message)
-        fetcher.write_line(payload)
+        connection.write_line(payload)
+      end
+
+      def process(*args)
+        init_srv
+        super
       end
 
     end
