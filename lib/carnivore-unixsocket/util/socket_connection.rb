ab8d3b746ae6450ac5164fadf1b525e89033499a
chrisroberts <chrisroberts.code@gmail.com>
2013-11-11 13:54:57 -0800

    Add connection helper to interactions with socket

---
+++ lib/carnivore-unixsocket/util/socket_connection.rb
@@ -0,0 +1,48 @@
+require 'celluloid/io'
+
+module Carnivore
+  module UnixSocket
+    module Util
+      class Connection
+
+        include Celluloid
+
+        attr_reader :srv, :io
+
+        def initialize(args={})
+          @io = args[:io]
+          @io.sync
+          @srv = args[:server]
+          async.consume! if args[:auto_consume]
+        end
+
+        def consume!
+          defer do
+            loop do
+              line = receive
+              unless(line.empty?)
+                srv.add_lines([line])
+              end
+            end
+          end
+        end
+
+        def send(s)
+          io.puts s
+          io.flush
+        end
+
+        def receive
+          line = io.gets
+          if(line)
+            line.strip
+          else
+            io.close
+            teminate
+          end
+        end
+
+      end
+    end
+  end
+end
