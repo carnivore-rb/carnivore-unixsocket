eb90a53540b7a96952f71dff49fd4f4636b19ef4
Chris Roberts <code@chrisroberts.org>
2017-01-28 10:58:24 -0800

    Update files for release v0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.2.2
+* Fix service namespace access
+* Allow connections access via reader
+
 # v0.2.0
 * Remove celluloid dependency
 
--- lib/carnivore-unixsocket/version.rb
+++ lib/carnivore-unixsocket/version.rb
@@ -1,6 +1,6 @@
 module Carnivore
   module UnixSocket
     # current library version
-    VERSION = Gem::Version.new('0.2.1')
+    VERSION = Gem::Version.new('0.2.2')
   end
 end
